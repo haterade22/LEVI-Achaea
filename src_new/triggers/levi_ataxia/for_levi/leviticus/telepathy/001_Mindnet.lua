190454224bc5b5998afb8adca08756768068ed1a
haterade22 <mikewilli22@gmail.com>
2026-01-03 13:32:25 -0600

    feat: Refactor Mindnet trigger logic for improved area checks and alerts

--- src_new/triggers/levi_ataxia/for_levi/leviticus/telepathy/001_Mindnet.lua
+++ src_new/triggers/levi_ataxia/for_levi/leviticus/telepathy/001_Mindnet.lua
@@ -42,18 +42,23 @@ end
 
 if ataxia_paused() then return end
 
-if ataxiaNDB.players[matches[2]].city ~= "Mhaldor" then
-    send("pt "..matches[2].." "..(matches[3] == "entered" and "IN!" or " IS OUT!"))
-    ataxiaBasher_alert("Normal")
-end
-
-
---if table.contains(ataxiaNDB.cityEnemies, matches[2]) then
-  -- send("pt "..matches[2].." "..(matches[3] == "entered" and "IN!" or "OUT!"))
- if ataxiaNDB_getCitizenship(ataxiaTemp.me) == gmcp.Room.Info.area and ataxiaNDB.players[matches[2]].city ~= "Mhaldor" then
+-- Check if raid mode is enabled (report everyone)
+if ataxia.settings.raid and ataxia.settings.raid.enabled then
+  send("pt "..matches[2].." "..(matches[3] == "entered" and "IN!" or "OUT!"))
+  ataxiaBasher_alert("Normal")
+-- Only process if we're in Mhaldor or Annwyn
+elseif gmcp.Room.Info.area == "Mhaldor" then
+  -- Only alert for city enemies in Mhaldor
+  if table.contains(ataxiaNDB.cityEnemies, matches[2]) then
+    send("pt "..matches[2].." "..(matches[3] == "entered" and "IN!" or "OUT!"))
     send("clt6 "..matches[2].." "..(matches[3] == "entered" and "has entered" or "has left").." "..gmcp.Room.Info.area)
     ataxiaBasher_alert("Normal")
   end
+elseif gmcp.Room.Info.area == "Annwyn" then
+  -- Alert for everyone in Annwyn (free PK area)
+  send("pt "..matches[2].." "..(matches[3] == "entered" and "IN!" or "OUT!"))
+  ataxiaBasher_alert("Normal")
+end
 
 
 if ataxiaTemp.enemies and table.contains(ataxiaTemp.enemies, matches[2]) then
