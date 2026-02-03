e7466ead41ea913068f3dc15578dda9d4db9a2e2
haterade22 <mikewilli22@gmail.com>
2026-02-03 10:53:19 -0600

    Refactor affliction handling in triggers to safely retrieve venom names; improve party relay messages for Depression, Retribution, Madness, and Leach scripts

--- src_new/triggers/levi_ataxia/for_levi/leviticus/429_Madness.lua
+++ src_new/triggers/levi_ataxia/for_levi/leviticus/429_Madness.lua
@@ -38,22 +38,25 @@ patterns:
 ]]--
 
 if isTargeted(matches[2]) then
-	Target_Instill = "madness"
-	if not haveAff("shadowmadness") then
-		tarAffed("shadowmadness")
-		if applyAffV3 then applyAffV3("shadowmadness") end
-    if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..envenomList[1].. " and shadowmadness") end
-    if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and shadowmadness") end
-	elseif not haveAff("vertigo") then
-		tarAffed("vertigo")
-		if applyAffV3 then applyAffV3("vertigo") end
-    if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..envenomList[1].. " and vertigo") end
-    if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and vertigo") end
-	else
-		tarAffed("hallucinations")
-		if applyAffV3 then applyAffV3("hallucinations") end
-    if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..envenomList[1].. " and hallucinations") end
-    if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and hallucinations") end
-	end
+    Target_Instill = "madness"
+    -- Guard: get venom name safely (may be nil when using timeloop)
+    local venomName = (envenomList and envenomList[1]) or "unknown"
+
+    if not haveAff("shadowmadness") then
+        tarAffed("shadowmadness")
+        if applyAffV3 then applyAffV3("shadowmadness") end
+        if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..venomName.. " and shadowmadness") end
+        if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and shadowmadness") end
+    elseif not haveAff("vertigo") then
+        tarAffed("vertigo")
+        if applyAffV3 then applyAffV3("vertigo") end
+        if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..venomName.. " and vertigo") end
+        if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and vertigo") end
+    else
+        tarAffed("hallucinations")
+        if applyAffV3 then applyAffV3("hallucinations") end
+        if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..venomName.. " and hallucinations") end
+        if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and hallucinations") end
+    end
 end
 
--- src_new/triggers/levi_ataxia/for_levi/leviticus/472_Retribution.lua
+++ src_new/triggers/levi_ataxia/for_levi/leviticus/472_Retribution.lua
@@ -38,16 +38,19 @@ patterns:
 ]]--
 
 if isTargeted(matches[2]) then
-	Target_Instill = "retribution"
-	if not haveAff("justice") then
-		tarAffed("justice")
-		if applyAffV3 then applyAffV3("justice") end
-    if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..envenomList[1].. " and justice") end
-    if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and justice") end
-	else
-		tarAffed("retribution")
-		if applyAffV3 then applyAffV3("retribution") end
-    if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..envenomList[1].. " and retribution") end
-    if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and retribution") end
-	end
+    Target_Instill = "retribution"
+    -- Guard: get venom name safely (may be nil when using timeloop)
+    local venomName = (envenomList and envenomList[1]) or "unknown"
+
+    if not haveAff("justice") then
+        tarAffed("justice")
+        if applyAffV3 then applyAffV3("justice") end
+        if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..venomName.. " and justice") end
+        if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and justice") end
+    else
+        tarAffed("retribution")
+        if applyAffV3 then applyAffV3("retribution") end
+        if partyrelay and tloop == false and tloop2 == false then send("pt "..target..": " ..venomName.. " and retribution") end
+        if partyrelay and (tloop == true or tloop2 == true) then send("pt "..target..": timeloop and retribution") end
+    end
 end
