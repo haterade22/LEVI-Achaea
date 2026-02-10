45c3102a36095915ae9bee7b983fb04f95a622b5
haterade22 <mikewilli22@gmail.com>
2026-02-10 13:51:41 -0600

    Add Impulse Suggestion and Impulse Relapse triggers for enhanced affliction tracking

---
+++ src_new/triggers/levi_ataxia/for_levi/leviticus/serpent/017_Impulse_Suggestion.lua
@@ -0,0 +1,43 @@
+--[[mudlet
+type: trigger
+name: Impulse Suggestion
+hierarchy:
+- Levi_Ataxia
+- For Levi
+- leviticus
+- Ataxia
+- Combat/Aff Tracking
+- Add Afflictions
+- Classes K-S
+- Serpent
+attributes:
+  isActive: 'yes'
+  isFolder: 'no'
+  isTempTrigger: 'no'
+  isMultiline: 'no'
+  isPerlSlashGOption: 'no'
+  isColorizerTrigger: 'no'
+  isFilterTrigger: 'no'
+  isSoundTrigger: 'no'
+  isColorTrigger: 'no'
+  isColorTriggerFg: 'no'
+  isColorTriggerBg: 'no'
+triggerType: 0
+conditonLineDelta: 0
+mStayOpen: 0
+mCommand: ''
+packageName: ''
+mFgColor: '#ff0000'
+mBgColor: '#ffff00'
+mSoundFile: ''
+colorTriggerFgColor: '#000000'
+colorTriggerBgColor: '#000000'
+patterns:
+- pattern: ^You craft the impulse of (\w+) in the mind of (\w+)\.$
+  type: 1
+]]--
+
+if isTargeted(matches[3]) then
+    tarAffed(matches[2])
+    if applyAffV3 then applyAffV3(matches[2]) end
+end
---
+++ src_new/triggers/levi_ataxia/for_levi/leviticus/serpent/018_Impulse_Relapse.lua
@@ -0,0 +1,44 @@
+--[[mudlet
+type: trigger
+name: Impulse Relapse
+hierarchy:
+- Levi_Ataxia
+- For Levi
+- leviticus
+- Ataxia
+- Combat/Aff Tracking
+- Add Afflictions
+- Classes K-S
+- Serpent
+attributes:
+  isActive: 'yes'
+  isFolder: 'no'
+  isTempTrigger: 'no'
+  isMultiline: 'no'
+  isPerlSlashGOption: 'no'
+  isColorizerTrigger: 'no'
+  isFilterTrigger: 'no'
+  isSoundTrigger: 'no'
+  isColorTrigger: 'no'
+  isColorTriggerFg: 'no'
+  isColorTriggerBg: 'no'
+triggerType: 0
+conditonLineDelta: 0
+mStayOpen: 0
+mCommand: ''
+packageName: ''
+mFgColor: '#ff0000'
+mBgColor: '#ffff00'
+mSoundFile: ''
+colorTriggerFgColor: '#000000'
+colorTriggerBgColor: '#000000'
+patterns:
+- pattern: ^You sense the remnants of your (\w+) impulse clawing at the psyche of (\w+)\.$
+  type: 1
+]]--
+
+if isTargeted(matches[3]) then
+    serpent.impulseRelapsing = true
+    tarAffed(matches[2])
+    if applyAffV3 then applyAffV3(matches[2]) end
+end
